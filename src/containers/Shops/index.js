42b8c6c2298e1ef801fefee0c008eb79b80bfa6c
Athman Gude <athmangude@gmail.com>
2017-02-20 18:17:16 +0300

    binding state and action creators to props

--- src/containers/Shops/index.js
+++ src/containers/Shops/index.js
@@ -1,6 +1,19 @@
 import React, { Component, PropTypes } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import * as shopsActions from './flux/actions';
+
+@connect(
+  state => ({ // eslint-disable-line arrow-parens
+    ...state.toJS(),
+  }),
+  dispatch => ({ // eslint-disable-line arrow-parens
+    shopsActions: bindActionCreators(shopsActions, dispatch),
+    dispatch,
+  }),
+)
 export default class Shops extends Component {
   render() {
     return (
