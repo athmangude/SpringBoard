3ea268611ee639f6cf44c770e56f74ef87743591
Athman Gude <athmangude@gmail.com>
2017-02-23 10:40:56 +0300

    added uiTheme

---
+++ src/uiTheme.js
@@ -0,0 +1,13 @@
+import { COLOR } from 'react-native-material-ui';
+
+export default uiTheme = {
+  palette: {
+    primaryColor: COLOR.green200,
+    accentColor: COLOR.orange800,
+  },
+  toolbar: {
+    container: {
+      height: 50,
+    },
+  },
+};
