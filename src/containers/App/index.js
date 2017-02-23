69f3b13f5926b550c337a4aba66c0b0126de78da
Athman Gude <athmangude@gmail.com>
2017-02-23 10:40:36 +0300

    using app navigator

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { StackNavigator } from 'react-navigation';
+import { ThemeProvider } from 'react-native-material-ui';
 import configureStore from '../../store';
+import uiTheme from '../../uiTheme';
 
 const store = configureStore();
 
 import Shops from '../Shops';
 
+const AppNavigator = StackNavigator({
+  Shops: { screen: Shops },
+});
+
 export default class App extends Component {
   render() {
     return (
+      <ThemeProvider theme={uiTheme}>
         <Provider store={store}>
-          <Shops />
+          <AppNavigator />
         </Provider>
+      </ThemeProvider>
     );
   }
 }
